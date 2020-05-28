8beb4db490bbc3a6365fbf23eaec79e039a42d31
ahmed-elnashar <a7medelnashar@hotmail.com>
2020-05-29 00:46:25 +0200

    Updates

---
+++ precache-manifest.29df56e0e6edc9ad929cf8d90d047c8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c1f99f7a7cdc3473ae777ea8f41353",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b05ebbba50612273824f",
+    "url": "/monsters-rolodex/static/css/main.f3e2c57c.chunk.css"
+  },
+  {
+    "revision": "76e1e33d0dc86b7a1eb3",
+    "url": "/monsters-rolodex/static/js/2.2e94f083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.2e94f083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b05ebbba50612273824f",
+    "url": "/monsters-rolodex/static/js/main.62a56ee0.chunk.js"
+  },
+  {
+    "revision": "f9890c86000462b21f2c",
+    "url": "/monsters-rolodex/static/js/runtime-main.a00addf9.js"
+  }
+]);
